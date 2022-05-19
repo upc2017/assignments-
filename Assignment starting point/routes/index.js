ce51e612e84d1f83f864e579a0e597076213b8a6
jota666888 <527093936@qq.com>
2022-05-19 03:46:45 +0100

    mongoDB

--- Assignment starting point/routes/index.js
+++ Assignment starting point/routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var Document = require('../models/Documents')
 var router = express.Router();
+var List = require('../controllers/List');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Image Browsing' });
+	res.render('index', {
+		title: 'Image Browsing'
+	});
 });
-router.get('/list',function (req,res,next){
-  res.render('list')
+router.get('/list', function(req, res, next) {
+	res.render('list')
 })
+
+
+router.post('/list/add', List.insert)
+
 module.exports = router;
